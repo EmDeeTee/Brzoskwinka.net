d63a75b150d9a3cd22f238fb63d416d2ec354d66
Michał <m12@onet.com.pl>
2024-12-09 23:11:26 +0100

    peach img

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
 import { ArrowDownIcon, CheckCircle, Gem, Sword } from "lucide-vue-next";
-import { loadLanguageAsync  } from 'laravel-vue-i18n'; 
+import { loadLanguageAsync  } from 'laravel-vue-i18n';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import Icon from '@/Components/Icon.vue';
 
@@ -11,13 +11,13 @@ const props = defineProps<{
     laravelVersion: string;
     phpVersion: string;
     peachImg: string;
-}>();
+}>(); //:src="props.peachImg"
 </script>
 
 <template>
-  
+
     <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-      <img :src="props.peachImg" alt="peach" width=40 height=30 />
+      <img :src="'/peach.webp' " alt="peach" width=40 height=30 />
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl ">
         {{$t('lp.header')}}
         <span className="text-orange-600">Brzoskwinka.Net</span>
@@ -26,7 +26,7 @@ const props = defineProps<{
         <Link href='/products' class="bg-orange-600 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">{{$t('lp.store.button')}}</Link>
       </div>
     </div>
-  
+
 </template>
 
 <script lang="ts">
